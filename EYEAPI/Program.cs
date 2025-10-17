d174a92443a42d4db94e15e98ce486bf13144a62
Elias <elias.andersen@hessel.dk>
2025-10-17 10:02:58 +0200

    fixed allow any caused by allow credentials in Program.cs

--- EYEAPI/Program.cs
+++ EYEAPI/Program.cs
@@ -94,8 +94,7 @@ builder.Services.AddCors(options =>
     {
         policy.AllowAnyOrigin()
             .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
+            .AllowAnyMethod();
     });
 });
 
